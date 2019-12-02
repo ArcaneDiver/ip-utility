051a094750e59c88012372909c5f4e50f2a171f6
ArcaneDiver <michele.dellamea@bearzi.org>
2019-12-02 15:59:42 +0100

    lint fixes

--- src/utility.ts
+++ src/utility.ts
@@ -1,6 +1,5 @@
 export const subStringInArrOfStr = (arg: string[], toBeSearched: string): string[] => {
         const res: string[] = [];
-        
         for (const e of arg) {
                 if (e.search(toBeSearched) !== -1) {
                         res.push(e);
